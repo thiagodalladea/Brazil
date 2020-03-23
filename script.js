8b4e919d058cf01980daf2424539a05576be351d
Thiago Dalla Dea <30993134+thiagodalladea@users.noreply.github.com>
2020-03-23 18:09:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,332 @@
+var ToF = true;
+//BAHIA
+var estBahia = document.querySelector("[name=bahia]");
+estBahia.style.transition = "all 0.3s";
+estBahia.style.fill = "#3d3d3d";
+estBahia.onmouseover = function() {
+  estBahia.style.fill = "#185fa1";
+};
+estBahia.onmouseout = function() {
+  estBahia.style.fill = "#3d3d3d";
+};
+//SERGIPE
+var estSergipe = document.querySelector("[name=sergipe]");
+estSergipe.style.transition = "all 0.3s";
+estSergipe.style.fill = "#3d3d3d";
+estSergipe.onmouseover = function() {
+  estSergipe.style.fill = "#185fa1";
+};
+estSergipe.onmouseout = function() {
+  estSergipe.style.fill = "#3d3d3d";
+};
+//PERNAMBUCO
+var estPernambuco = document.querySelector("[name=pernambuco]");
+estPernambuco.style.transition = "all 0.3s";
+estPernambuco.style.fill = "#3d3d3d";
+estPernambuco.onmouseover = function() {
+  estPernambuco.style.fill = "#185fa1";
+};
+estPernambuco.onmouseout = function() {
+  estPernambuco.style.fill = "#3d3d3d";
+};
+//ALAGOAS
+var estAlagoas = document.querySelector("[name=alagoas]");
+estAlagoas.style.transition = "all 0.3s";
+estAlagoas.style.fill = "#3d3d3d";
+estAlagoas.onmouseover = function() {
+  estAlagoas.style.fill = "#185fa1";
+};
+estAlagoas.onmouseout = function() {
+  estAlagoas.style.fill = "#3d3d3d";
+};
+//RIO GRANDE DO NORTE
+var estRiograndedonorte = document.querySelector("[name=riograndedonorte]");
+estRiograndedonorte.style.transition = "all 0.3s";
+estRiograndedonorte.style.fill = "#3d3d3d";
+estRiograndedonorte.onmouseover = function() {
+  estRiograndedonorte.style.fill = "#185fa1";
+};
+estRiograndedonorte.onmouseout = function() {
+  estRiograndedonorte.style.fill = "#3d3d3d";
+};
+//CEARA
+var estCeara = document.querySelector("[name=ceara]");
+estCeara.style.transition = "all 0.3s";
+estCeara.style.fill = "#3d3d3d";
+estCeara.onmouseover = function() {
+  estCeara.style.fill = "#185fa1";
+};
+estCeara.onmouseout = function() {
+  estCeara.style.fill = "#3d3d3d";
+};
+//PIAUI
+var estPiaui = document.querySelector("[name=piaui]");
+estPiaui.style.transition = "all 0.3s";
+estPiaui.style.fill = "#3d3d3d";
+estPiaui.onmouseover = function() {
+  estPiaui.style.fill = "#185fa1";
+};
+estPiaui.onmouseout = function() {
+  estPiaui.style.fill = "#3d3d3d";
+};
+//MARANHAO
+var estMaranhao = document.querySelector("[name=maranhao]");
+estMaranhao.style.transition = "all 0.3s";
+estMaranhao.style.fill = "#3d3d3d";
+estMaranhao.onmouseover = function() {
+  estMaranhao.style.fill = "#185fa1";
+};
+estMaranhao.onmouseout = function() {
+  estMaranhao.style.fill = "#3d3d3d";
+};
+//PARAIBA
+var estParaiba = document.querySelector("[name=paraiba]");
+estParaiba.style.transition = "all 0.3s";
+estParaiba.style.fill = "#3d3d3d";
+estParaiba.onmouseover = function() {
+  estParaiba.style.fill = "#185fa1";
+};
+estParaiba.onmouseout = function() {
+  estParaiba.style.fill = "#3d3d3d";
+};
+//TOCANTINS
+var estTocantins = document.querySelector("[name=tocantins]");
+estTocantins.style.transition = "all 0.3s";
+estTocantins.style.fill = "#3d3d3d";
+estTocantins.onmouseover = function() {
+  estTocantins.style.fill = "#18a138";
+};
+estTocantins.onmouseout = function() {
+  estTocantins.style.fill = "#3d3d3d";
+};
+//AMAPA
+var estAmapa = document.querySelector("[name=amapa]");
+estAmapa.style.transition = "all 0.3s";
+estAmapa.style.fill = "#3d3d3d";
+estAmapa.onmouseover = function() {
+  estAmapa.style.fill = "#18a138";
+};
+estAmapa.onmouseout = function() {
+  estAmapa.style.fill = "#3d3d3d";
+};
+//PARA
+var estPara = document.querySelector("[name=para]");
+estPara.style.transition = "all 0.3s";
+estPara.style.fill = "#3d3d3d";
+estPara.onmouseover = function() {
+  estPara.style.fill = "#18a138";
+};
+estPara.onmouseout = function() {
+  estPara.style.fill = "#3d3d3d";
+};
+//RORAIMA
+var estRoraima = document.querySelector("[name=roraima]");
+estRoraima.style.transition = "all 0.3s";
+estRoraima.style.fill = "#3d3d3d";
+estRoraima.onmouseover = function() {
+  estRoraima.style.fill = "#18a138";
+};
+estRoraima.onmouseout = function() {
+  estRoraima.style.fill = "#3d3d3d";
+};
+//AMAZONAS
+var estAmazonas = document.querySelector("[name=amazonas]");
+estAmazonas.style.transition = "all 0.3s";
+estAmazonas.style.fill = "#3d3d3d";
+estAmazonas.onmouseover = function() {
+  estAmazonas.style.fill = "#18a138";
+};
+estAmazonas.onmouseout = function() {
+  estAmazonas.style.fill = "#3d3d3d";
+};
+//ACRE
+var estAcre = document.querySelector("[name=acre]");
+estAcre.style.transition = "all 0.3s";
+estAcre.style.fill = "#3d3d3d";
+estAcre.onmouseover = function() {
+  estAcre.style.fill = "#18a138";
+};
+estAcre.onmouseout = function() {
+  estAcre.style.fill = "#3d3d3d";
+};
+//RONDONIA
+var estRondonia = document.querySelector("[name=rondonia]");
+estRondonia.style.transition = "all 0.3s";
+estRondonia.style.fill = "#3d3d3d";
+estRondonia.onmouseover = function() {
+  estRondonia.style.fill = "#18a138";
+};
+estRondonia.onmouseout = function() {
+  estRondonia.style.fill = "#3d3d3d";
+};
+//MATOGROSSO
+var estMatogrosso = document.querySelector("[name=matogrosso]");
+estMatogrosso.style.transition = "all 0.3s";
+estMatogrosso.style.fill = "#3d3d3d";
+estMatogrosso.onmouseover = function() {
+  estMatogrosso.style.fill = "#ffd83b";
+};
+estMatogrosso.onmouseout = function() {
+  estMatogrosso.style.fill = "#3d3d3d";
+};
+//MATOGROSSODOSUL
+var estMatogrossodosul = document.querySelector("[name=matogrossodosul]");
+estMatogrossodosul.style.transition = "all 0.3s";
+estMatogrossodosul.style.fill = "#3d3d3d";
+estMatogrossodosul.onmouseover = function() {
+  estMatogrossodosul.style.fill = "#ffd83b";
+};
+estMatogrossodosul.onmouseout = function() {
+  estMatogrossodosul.style.fill = "#3d3d3d";
+};
+//GOIAS
+var estGoias = document.querySelector("[name=goias]");
+estGoias.style.transition = "all 0.3s";
+estGoias.style.fill = "#3d3d3d";
+estGoias.onmouseover = function() {
+  estGoias.style.fill = "#ffd83b";
+};
+estGoias.onmouseout = function() {
+  estGoias.style.fill = "#3d3d3d";
+};
+//DISTRITO FEDERAL
+var estDistritofederal = document.querySelector("[name=distritofederal]");
+estDistritofederal.style.transition = "all 0.3s";
+estDistritofederal.style.fill = "#3d3d3d";
+estDistritofederal.onmouseover = function() {
+  estDistritofederal.style.fill = "#ffd83b";
+};
+estDistritofederal.onmouseout = function() {
+  estDistritofederal.style.fill = "#3d3d3d";
+};
+//PARANA
+var estParana = document.querySelector("[name=parana]");
+estParana.style.transition = "all 0.3s";
+estParana.style.fill = "#3d3d3d";
+estParana.onmouseover = function() {
+  estParana.style.fill = "#6618a1";
+};
+estParana.onmouseout = function() {
+  estParana.style.fill = "#3d3d3d";
+};
+//SANTA CATARINA
+var estSantaCatarina = document.querySelector("[name=santacatarina]");
+estSantaCatarina.style.transition = "all 0.3s";
+estSantaCatarina.style.fill = "#3d3d3d";
+estSantaCatarina.onmouseover = function() {
+  estSantaCatarina.style.fill = "#6618a1";
+};
+estSantaCatarina.onmouseout = function() {
+  estSantaCatarina.style.fill = "#3d3d3d";
+};
+//RIO GRANDE DO SUL
+var estRiograndedosul = document.querySelector("[name=riograndedosul]");
+estRiograndedosul.style.transition = "all 0.3s";
+estRiograndedosul.style.fill = "#3d3d3d";
+estRiograndedosul.onmouseover = function() {
+  estRiograndedosul.style.fill = "#6618a1";
+};
+estRiograndedosul.onmouseout = function() {
+  estRiograndedosul.style.fill = "#3d3d3d";
+};
+//SAO PAULO
+var estSaopaulo = document.querySelector("[name=saopaulo]");
+estSaopaulo.style.transition = "all 0.3s";
+estSaopaulo.style.fill = "#3d3d3d";
+estSaopaulo.onmouseover = function() {
+  estSaopaulo.style.fill = "#ed134d";
+};
+estSaopaulo.onmouseout = function() {
+  estSaopaulo.style.fill = "#3d3d3d";
+};
+//MINAS GERAIS
+var estMinasgerais = document.querySelector("[name=minasgerais]");
+estMinasgerais.style.transition = "all 0.3s";
+estMinasgerais.style.fill = "#3d3d3d";
+estMinasgerais.onmouseover = function() {
+  estMinasgerais.style.fill = "#ed134d";
+};
+estMinasgerais.onmouseout = function() {
+  estMinasgerais.style.fill = "#3d3d3d";
+};
+//RIO DE JANEIRO
+var estRiodejaneiro = document.querySelector("[name=riodejaneiro]");
+estRiodejaneiro.style.transition = "all 0.3s";
+estRiodejaneiro.style.fill = "#3d3d3d";
+estRiodejaneiro.onmouseover = function() {
+  estRiodejaneiro.style.fill = "#ed134d";
+};
+estRiodejaneiro.onmouseout = function() {
+  estRiodejaneiro.style.fill = "#3d3d3d";
+};
+//ESPIRITO SANTO
+var estEspiritosanto = document.querySelector("[name=espiritosanto]");
+estEspiritosanto.style.transition = "all 0.3s";
+estEspiritosanto.style.fill = "#3d3d3d";
+estEspiritosanto.onmouseover = function() {
+  estEspiritosanto.style.fill = "#ed134d";
+};
+estEspiritosanto.onmouseout = function() {
+  estEspiritosanto.style.fill = "#3d3d3d";
+};
+function todasCores() {
+  if (ToF == true) {
+    estRiodejaneiro.style.fill = "#ed134d";
+    estSaopaulo.style.fill = "#ed134d";
+    estEspiritosanto.style.fill = "#ed134d";
+    estMinasgerais.style.fill = "#ed134d";
+    estRiograndedosul.style.fill = "#6618a1";
+    estParana.style.fill = "#6618a1";
+    estSantaCatarina.style.fill = "#6618a1";
+    estGoias.style.fill = "#ffd83b";
+    estMatogrosso.style.fill = "#ffd83b";
+    estMatogrossodosul.style.fill = "#ffd83b";
+    estDistritofederal.style.fill = "#ffd83b";
+    estTocantins.style.fill = "#18a138";
+    estPara.style.fill = "#18a138";
+    estRondonia.style.fill = "#18a138";
+    estAmapa.style.fill = "#18a138";
+    estAmazonas.style.fill = "#18a138";
+    estAcre.style.fill = "#18a138";
+    estRoraima.style.fill = "#18a138";
+    estBahia.style.fill = "#185fa1";
+    estPiaui.style.fill = "#185fa1";
+    estParaiba.style.fill = "#185fa1";
+    estPernambuco.style.fill = "#185fa1";
+    estRiograndedonorte.style.fill = "#185fa1";
+    estCeara.style.fill = "#185fa1";
+    estMaranhao.style.fill = "#185fa1";
+    estSergipe.style.fill = "#185fa1";
+    estAlagoas.style.fill = "#185fa1";
+    ToF = false;
+  } else if (ToF == false) {
+    estRiodejaneiro.style.fill = "#3d3d3d";
+    estSaopaulo.style.fill = "#3d3d3d";
+    estEspiritosanto.style.fill = "#3d3d3d";
+    estMinasgerais.style.fill = "#3d3d3d";
+    estRiograndedosul.style.fill = "#3d3d3d";
+    estParana.style.fill = "#3d3d3d";
+    estSantaCatarina.style.fill = "#3d3d3d";
+    estGoias.style.fill = "#3d3d3d";
+    estMatogrosso.style.fill = "#3d3d3d";
+    estMatogrossodosul.style.fill = "#3d3d3d";
+    estDistritofederal.style.fill = "#3d3d3d";
+    estTocantins.style.fill = "#3d3d3d";
+    estPara.style.fill = "#3d3d3d";
+    estRondonia.style.fill = "#3d3d3d";
+    estAmapa.style.fill = "#3d3d3d";
+    estAmazonas.style.fill = "#3d3d3d";
+    estAcre.style.fill = "#3d3d3d";
+    estRoraima.style.fill = "#3d3d3d";
+    estBahia.style.fill = "#3d3d3d";
+    estPiaui.style.fill = "#3d3d3d";
+    estParaiba.style.fill = "#3d3d3d";
+    estPernambuco.style.fill = "#3d3d3d";
+    estRiograndedonorte.style.fill = "#3d3d3d";
+    estCeara.style.fill = "#3d3d3d";
+    estMaranhao.style.fill = "#3d3d3d";
+    estSergipe.style.fill = "#3d3d3d";
+    estAlagoas.style.fill = "#3d3d3d";
+    ToF = true;
+  }
+}
